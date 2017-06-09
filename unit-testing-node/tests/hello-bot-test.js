dc56894b838bb204e28ef2ccfdf258fe3928273c
erdivartanovich <erdivartanovich@gmail.com>
2017-06-09 17:56:14 +0700

    update example

--- unit-testing-node/tests/hello-bot-test.js
+++ unit-testing-node/tests/hello-bot-test.js
@@ -4,13 +4,13 @@ var HelloBot = require('./../hello-bot');
 var helloBot = new HelloBot();
 
 describe('Hello Bot Testing', function() {
-    it('greeting should return ' + helloBot.hello_const, function() {
-        expect(helloBot.greeting).to.equal(hello_const);
+    it('greeting should return Hello string', function() {
+        expect(helloBot.greeting).to.equal('Hello ');
     });
 
     //you can have another method test down here
     //for example
-    // it('greetingToPerson should return ' + helloBot.hello_const + helloBot._person, function() {
-    //     expect(helloBot.greetingToPerson).to.equal(hello_const + helloBot._person);
+    // it('greetingToPerson should return Hello to person', function() {
+    //     expect(helloBot.greetingToPerson).to.equal('Hello person');
     // }); 
 });
